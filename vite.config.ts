f10d090f9aea221921f0a4dee06912fac4c613c6
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2026-02-15 19:52:03 +0800

    适配了PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import i18next from "vite-plugin-i18next-loader";
 import svgr from "vite-plugin-svgr";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
+import { VitePWA } from "vite-plugin-pwa";
 import jotaiDebugLabel from "jotai/babel/plugin-debug-label";
 import jotaiReactRefresh from "jotai/babel/plugin-react-refresh";
 
@@ -25,6 +26,34 @@ export default defineConfig({
 		svgr(),
 		wasm(),
 		topLevelAwait(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ["favicon.png", "logo.svg"],
+			manifest: {
+				name: "Steve Taiko Configurator",
+				short_name: "TaikoConfig",
+				description: "A tool to configure Steve Taiko controllers",
+				theme_color: "#111113",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "logo.svg",
+						sizes: "any",
+						type: "image/svg+xml",
+						purpose: "any",
+					},
+				],
+			},
+		}),
 	],
 	envPrefix: ["VITE_", "TAURI_"],
 
